ac25c2a067a5fbaf2b564258079973003cf384b2
Krish <krshrg@gmail.com>
2023-10-17 20:33:21 -0700

    Update Class 1 Exercises.js
    removed unnecessary spaces between lines of code - Krish

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -1,32 +1,24 @@
 /**
  * MATH
  */
-
 // 1. Pagliacci charges $16.99 for a 13” pizza and $19.99 for a 17” pizza.
 // What is the area for each of these pizzas?
 // (radius would be the listed size - i.e. 13" - divided by 2)
-
 let smallPizzaDiameter = 13
 let largePizzaDiameter = 17
-
 console.log(("The area of the small pizza is "),Math.round(smallPizzaArea = Math.PI * ((smallPizzaDiameter/2)**2)))
 console.log(("The area of the large pizza is "),Math.round(largePizzaArea = Math.PI * ((largePizzaDiameter/2)**2)))
-
 // 2. What is the cost per square inch of each pizza?
 const smallPizzaCost = 16.99
 const largePizzaCost = 19.99
-
 smallPizzaCostPerSquareInch = smallPizzaCost / smallPizzaArea
 largePizzaCostPerSquareInch = largePizzaCost / largePizzaArea
-
 console.log(("The cost per square inch of the small pizza is $"), (smallPizzaCostPerSquareInch).toFixed(2))
 console.log(("The cost per square inch of the large pizza is $"), (largePizzaCostPerSquareInch).toFixed(2))
-
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
 console.log(`Math.round(Math.random()*13)`)
-
 // 4. Draw 3 cards and use Math to determine the highest
 // card
 Card1 = Math.round(Math.random()*13)
@@ -39,7 +31,6 @@ console.log(("The highest card is "),Math.max(Card1,Card2,Card3))
 /**
  * ADDRESS LINE
  */
-
 // 1. Create variables for firstName, lastName,
 // streetAddress, city, state, and zipCode. Use
 // this information to create a formatted address block
@@ -52,7 +43,6 @@ let state = 'CA'
 let zipCode = '90210'
 let address = (`${firstName} ${lastName}\n${streetAddress}\n${city}, ${state} ${zipCode}`)
 console.log(address)
-
 // 2. You are given a string in this format:
 // firstName lastName(assume no spaces in either)
 // streetAddress
@@ -60,7 +50,6 @@ console.log(address)
 // 
 // Write code that is able to extract the first name from this string into a variable.
 // Hint: use indexOf, slice, and / or substring
-
 console.log(address.indexOf(firstName))
 console.log(address.indexOf(lastName))
 const extractFirstName = address.slice(0, 6);
@@ -73,7 +62,6 @@ console.log(extractFirstName)
 // 1/1/2020 00:00:00 and 4/1/2020 00:00:00
 //
 // Look online for documentation on Date objects.
-
 // Starting hint:
 const endDate = new Date(2020, 3, 1);
 const startDate = new Date(2020, 0, 1);
